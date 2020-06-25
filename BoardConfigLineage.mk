0cd00b63d908fe40606af4a789a0338264813f80
bigbiff <bigbiff@teamw.in>
2020-06-25 19:17:25 -0400

    wahoo: add twrp variables to board

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -13,3 +13,20 @@ TARGET_NEEDS_DTBOIMAGE := true
 # Build vendor image
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_PARTITION_SIZE := 524288000
+
+# TWRP
+TW_THEME := portrait_hdpi
+BOARD_SUPPRESS_SECURE_ERASE := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_DEFAULT_BRIGHTNESS := "80"
+TW_INCLUDE_CRYPTO := true
+AB_OTA_UPDATER := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TARGET_RECOVERY_FSTAB := device/google/wahoo/recovery.fstab
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+# MTP will not work until we update it to support ffs
+TW_EXCLUDE_MTP := true
+PLATFORM_SECURITY_PATCH := 2020-10-05
+TW_OVERRIDE_SYSTEM_PROPS := "ro.build.version.security_patch"
